cd6d9e1db3054624cded588ae3565308c74dd789
jeremy rodriguez <jeremy12troll@gmail.com>
2025-10-18 12:29:27 -0600

    Se elimino estructura html al view user-register, ya que app.php tiene uno, ademas se agrego componentes pero sigue persistiendo un error de altura

--- src/views/user-register.php
+++ src/views/user-register.php
@@ -1,17 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>AVENTONES</title>
-</head>
-
-<body>
-
-    <div class="form-container bg-white dark:bg-gray-800 shadow-lg p-8">
-
-        <?php include COMP_PATH . '/toggle.php'; ?>
+<div class="bg-white dark:bg-gray-800 shadow-lg p-8 flex justify-center">
+    <div class="form-container bg-white dark:bg-gray-800 shadow-lg rounded-xl p-8 w-full max-w-2xl">
+        <div class="flex mb-3 items-center">
+            <div><?php include COMP_PATH . '/theme-toggle.php'; ?></div>
+            <div class="h-8"><?php include COMP_PATH . '/toggle.php'; ?></div>
+        </div>
+        <div class="justify-center flex">
+            <h1 class="font-bold text-2xl leading-[1.32]">
+                Create a Aventones Account
+            </h1>
+        </div>
         <form class="max-w-md mx-auto">
             <div class="grid md:grid-cols-2 md:gap-6">
                 <div class="relative z-0 w-full mb-5 group">
@@ -22,7 +19,6 @@
                         class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">First
                         name</label>
                 </div>
-
                 <div class="relative z-0 w-full mb-5 group">
                     <input type="text" name="floating_last_name" id="floating_last_name"
                         class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
@@ -87,10 +83,10 @@
             </div>
 
             <div class="relative z-0 w-full mb-5 group">
-                <input type="file" name="foto" id="foto" accept="image/*"
+                <input type="file" name="photo" id="photo" accept="image/*"
                     class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
                     required />
-                <label for="foto" class="block mt-2 text-sm font-medium text-gray-700 dark:text-gray-300">
+                <label for="photo" class="block mt-2 text-sm font-medium text-gray-700 dark:text-gray-300">
                     Fotografía personal
                 </label>
 
@@ -100,11 +96,9 @@
                         alt="Vista previa" />
                 </div> -->
             </div>
-           
+
             <button type="submit"
                 class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
         </form>
     </div>
-</body>
-
-</html>
+</div>
