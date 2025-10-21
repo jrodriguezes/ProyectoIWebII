a68b7a2189fadbbddee90397dcde27caf967ca0e
jeremy rodriguez <jeremy12troll@gmail.com>
2025-10-20 21:31:25 -0600

    Configuracion libreria composer(permite mandar emails)

---
+++ src/config/mailer.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+
+function make_mailer(): PHPMailer
+{
+    $m = new PHPMailer(true);
+
+    $m->isSMTP();                         // usa SMTP
+    $m->Host = 'smtp.gmail.com';    // servidor de Gmail
+    $m->SMTPAuth = true;                // requiere autenticación
+    $m->Username = 'noreplyaventones@gmail.com';
+    $m->Password = 'kdmuijgkjtrdwupq';
+    $m->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;   // TLS
+    $m->Port = 587;                 // puerto TLS
+
+    $m->CharSet = 'UTF-8';
+
+    $m->setFrom('noreplyaventones@gmail.com', 'Aventones');
+
+    return $m;
+}
