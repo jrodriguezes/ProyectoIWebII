62a785170037fc5722a5e0f9728dfb8ea1de050c
jeremy rodriguez <jeremy12troll@gmail.com>
2025-10-20 21:37:54 -0600

    Se modifico el el insert de usuario ya que se aplicaron cambios en la bd para poder verificar cuenta mediante email

--- src/models/InsertFunctions.php
+++ src/models/InsertFunctions.php
@@ -1,17 +1,17 @@
 <?php
 require_once __DIR__ . '/../config/db.php';
 
-function insertData($id, $first_name, $last_name, $birth_date, $email, $phone_number, $profile_photo, $password, $user_type, $stateId)
+function insertData($id, $first_name, $last_name, $birth_date, $email, $phone_number, $profile_photo, $password, $user_type, $stateId, $verify_hash, $verify_expires)
 {
     $conn = getConnection();
 
-    $sql = "INSERT INTO users (id, first_name, last_name, birth_date, email, phone_number, profile_photo, password, user_type, status) 
-            VALUES ('$id', '$first_name', '$last_name', '$birth_date', '$email', '$phone_number', '$profile_photo', '$password', '$user_type', '$stateId')";
+    $sql = "INSERT INTO users (id, first_name, last_name, birth_date, email, phone_number, profile_photo, password, user_type, status, verify_token_hash, verify_token_expires_at)
+            VALUES ('$id', '$first_name', '$last_name', '$birth_date', '$email', '$phone_number', '$profile_photo', '$password', '$user_type', '$stateId', '$verify_hash', '$verify_expires')";
 
     if ($conn->query($sql) == TRUE) {
         return true;
     } else {
-        return "Error: " . $conn->error;
+        return 'Error: ' . $conn->error;
     }
 
     $conn->close();
