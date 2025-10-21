5e3547fce24025679c48e366b6148118431ce395
jeremy rodriguez <jeremy12troll@gmail.com>
2025-10-20 21:40:04 -0600

    se elimino header

--- src/views/layouts/header.php
+++
@@ -1,5 +0,0 @@
-<header class="header bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100 shadow-md">
-  <?php include COMP_PATH . '/theme-toggle.php'; ?>
-  <img src="<?= BASE_URL ?>assets/img/AventonesLogo.png" alt="Logo" class="header-logo">
-  <h1 class="header bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100 shadow-md">Aventones</h1>
-</header>
