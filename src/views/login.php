8fe9fbcd6d99705df1e08df30e592bd4134f7aff
jeremy rodriguez <jeremy12troll@gmail.com>
2025-10-22 12:41:30 -0600

    Se configuro el form para poder redirigir a login.php y se coloco post de metodo

--- src/views/login.php
+++ src/views/login.php
@@ -7,7 +7,7 @@
             Welcome back to Aventones
         </h1>
     </div>
-    <form action="" class="flex flex-col items-center dark:bg-gray-800">
+    <form action="/post/login.php" method="POST" class="flex flex-col items-center dark:bg-gray-800">
         <div class="relative z-0 w-full mb-5 group m-8">
             <input type="email" name="floating_email" id="floating_email"
                 class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
