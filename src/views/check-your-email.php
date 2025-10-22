98817d5dc88ab287f1002282332aae413bf3b336
jeremy rodriguez <jeremy12troll@gmail.com>
2025-10-21 22:54:27 -0600

    Se creo una pagina basica para poder avisarle al usuario que revise su correo para verificar la cuenta

---
+++ src/views/check-your-email.php
@@ -0,0 +1,5 @@
+<div class="max-w-md mx-auto text-center py-10">
+  <h1 class="text-2xl font-bold mb-2">Revisa tu correo 📬</h1>
+  <p>Te enviamos un enlace para verificar tu cuenta. Revisa tu bandeja de entrada y también la carpeta de spam.</p>
+  <a href='/login' class="hover:text-blue-600">Ir a login</a>;
+</div>
