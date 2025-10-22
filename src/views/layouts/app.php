a70e9529bd72c7f063b33d3ec583be5fe5e44912
jeremy rodriguez <jeremy12troll@gmail.com>
2025-10-22 12:39:33 -0600

    Se declaro los datos del usuario globales para poder utilizarlo en las diferentes paginas home

--- src/views/layouts/app.php
+++ src/views/layouts/app.php
@@ -2,24 +2,28 @@
 <html lang="es">
 
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Aventones</title>
-    <link rel="stylesheet" href="<?= rtrim(BASE_URL, '/') ?>/assets/css/tailwind.css">
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Aventones</title>
+  <link rel="stylesheet" href="<?= rtrim(BASE_URL, '/') ?>/assets/css/tailwind.css">
 
-    <script>
-        const stored = localStorage.getItem('color-theme');
-        const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  <script>
+    const stored = localStorage.getItem('color-theme');
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
 
-        if (stored === 'dark' || (!stored && prefersDark)) {
-            document.documentElement.classList.add('dark');
-        } else {
-            document.documentElement.classList.remove('dark');
-        }
-    </script>
+    if (stored === 'dark' || (!stored && prefersDark)) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  </script>
 </head>
 
 <body class="min-h-screen flex flex-col bg-gray-50 dark:bg-gray-900 text-gray-100 overflow-x-hidden">
+  <?php
+  require_once __DIR__ . '/../../config/session.php';
+  $user = $_SESSION['user'] ?? null;
+  ?>
   <main class="flex-1 flex items-center justify-center">
     <?= $content ?>
   </main>
@@ -28,9 +32,8 @@
     <?php include LAYOUT_PATH . '/nav.php'; ?>
   </footer>
 
-    <script src="<?= rtrim(BASE_URL, '/') ?>/assets/js/theme-toggle.js"></script>
-    <script src="<?= rtrim(BASE_URL, '/') ?>/assets/js/toggle-user-type.js"></script>
+  <script src="<?= rtrim(BASE_URL, '/') ?>/assets/js/theme-toggle.js"></script>
+  <script src="<?= rtrim(BASE_URL, '/') ?>/assets/js/toggle-user-type.js"></script>
 </body>
 
 </html>
-
