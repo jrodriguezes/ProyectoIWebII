72176c81c09545b66cf7fe20f8882f23fa87c907
jeremy rodriguez <jeremy12troll@gmail.com>
2025-10-22 12:43:41 -0600

    Configuracion de login

---
+++ src/config/session.php
@@ -0,0 +1,11 @@
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_set_cookie_params([
+        'lifetime' => 0,
+        'path'     => '/',
+        'secure'   => !empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off',
+        'httponly' => true,
+        'samesite' => 'Lax',
+    ]);
+    session_start();
+}
