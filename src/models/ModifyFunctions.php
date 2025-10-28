802fe5e72cfe58cb1b50c106ddcd431691369e83
jeremy rodriguez <jeremy12troll@gmail.com>
2025-10-27 20:06:01 -0600

    funcion para modificar un vehiculo agregada

---
+++ src/models/ModifyFunctions.php
@@ -0,0 +1,30 @@
+<?php
+require_once __DIR__ . '/../config/db.php';
+
+function modifyVehicle($plate_id, $color, $brand, $model, $year, $seats, $vehicle_picture, $driver_id)
+{
+    $conn = getConnection();
+
+    $sql = "UPDATE vehicles SET 
+                color = '$color',
+                brand = '$brand',
+                model = '$model',
+                year = '$year',
+                seats = '$seats'";
+
+    if (!empty($vehicle_picture)) {
+        $sql .= ", vehicle_picture = '$vehicle_picture'";
+    }
+
+    $sql .= " WHERE plate_id = '$plate_id' AND driver_id = '$driver_id'";
+
+    if ($conn->query($sql) === TRUE) {
+        return true;
+    } else {
+        return 'Error: ' . $conn->error;
+    }
+
+    $conn->close();
+}
+
+?>
