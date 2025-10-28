9c687fcfc0d0015ef2a1edebcd6e5ea448149ff6
jeremy rodriguez <jeremy12troll@gmail.com>
2025-10-28 17:16:57 -0600

    Logout implementado en home, se removio un logoutcontroller inecesario, y se creo un logout en post

---
+++ public/post/logout.php
@@ -0,0 +1,15 @@
+<?php
+// /src/common/logout.php
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  session_unset();
+  session_destroy();
+  header("Location: /login", true, 303);
+  exit();
+} else {
+  // si alguien intenta entrar directo por GET
+  header("Location: /");
+  exit();
+}
+?>
