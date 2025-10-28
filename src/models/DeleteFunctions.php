424320dd8746002935a8f99a5cde9b1f6c441683
jeremy rodriguez <jeremy12troll@gmail.com>
2025-10-27 20:03:58 -0600

    Funcion para inhabilitar los vehiculos

---
+++ src/models/DeleteFunctions.php
@@ -0,0 +1,18 @@
+<?php
+require_once __DIR__ . '/../config/db.php';
+function deleteVehicle($plate_id, $driver_id)
+{
+    $conn = getConnection();
+
+    $sql = "UPDATE vehicles set status = 'inactive' where plate_id = '$plate_id' and driver_id = '$driver_id'";
+
+    if ($conn->query($sql) === TRUE) {
+        return true;
+    } else {
+        return false;
+    }
+    $conn->close();
+
+}
+
+?>
