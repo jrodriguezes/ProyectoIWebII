a83bc56dd3ad7ece1985cdb2b2d0b393ad8ce4f5
jeremy rodriguez <jeremy12troll@gmail.com>
2025-11-05 16:08:43 -0600

    Bugs fixed

--- src/models/UserModel.php
+++ src/models/UserModel.php
@@ -41,5 +41,38 @@ function updateProfile($user_id, $first_name, $last_name, $password, $phone_numb
     $conn->close();
 }
 
+require_once __DIR__ .'/../config/db.php';
+
+function getUsers() {
+    $conn = getConnection();
+
+    $sql = "SELECT 
+                id,
+                first_name,
+                last_name,
+                birth_date,
+                email,
+                phone_number,
+                profile_photo,
+                user_type,
+                status,
+                created_at
+            FROM users";
+
+    $result = $conn->query($sql);
+
+    $users = [];
+
+    if ($result && $result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $users[] = $row;
+        }
+    }
+
+    $conn->close();
+
+    return $users;
+}
+
 
 ?>
