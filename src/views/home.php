fc25a80399618e173e1ef731ef768ecaa90d056b
Mygyx <miguelcruzvargas12@gmail.com>
2025-11-06 21:40:53 -0600

    arreglar el home/ride que la fecha se ponga en el modify

--- src/views/home.php
+++ src/views/home.php
@@ -326,8 +326,14 @@ $searchRides = getAllRides();
                                                         class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                                                         Departure time
                                                     </label>
+                                                    <?php
+                                                    $datetimeValue = '';
+                                                    if (!empty($ride['departure_date'])) {
+                                                        $datetimeValue = date('Y-m-d\TH:i', strtotime($ride['departure_date']));
+                                                    }
+                                                    ?>
                                                     <input type="datetime-local" name="departure_date" id="departure_date"
-                                                        value="<?= htmlspecialchars($ride['departure_date'], 0, 5) /* HH:MM */ ?>"
+                                                        value="<?= htmlspecialchars($datetimeValue) ?>"
                                                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                                                         required>
                                                 </div>
